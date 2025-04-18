f6231bba2d9a4c3688fd4978fa2e598f0ac4daa3
bubblefish75 <bubblefish75@theself.cloud>
2025-04-18 12:07:34 -0500

    Small union fix

--- statement.go
+++ statement.go
@@ -258,12 +258,16 @@ func (s *Stmt) bindComplexValue(val driver.NamedValue, n int) (mapping.State, er
 		return state, nil
 	case TYPE_UNION:
 		switch u := val.Value.(type) {
+		// FIXME: will not work for unions that have multiple tags of the same type,
+		// blocked on duckdb exposing duckdb_create_union_type: https://github.com/duckdb/duckdb/pull/14613
 		case Union[any]:
 			return s.bindValue(driver.NamedValue{Name: val.Name, Ordinal: val.Ordinal, Value: u.MemberValue}, n)
+		default:
+			return mapping.StateError, addIndexToError(unsupportedTypeError(unknownTypeErrMsg), n+1)
 		}
 	case TYPE_TIMESTAMP_S, TYPE_TIMESTAMP_MS, TYPE_TIMESTAMP_NS, TYPE_MAP, TYPE_ENUM:
 		// FIXME: for timestamps: distinguish between timestamp[_s|ms|ns] once available.
-		// FIXME: for other types: duckdb_param_logical_type once available, then create duckdb_value + duckdb_bind_value
+		// FIXME: for maps: duckdb_create_map_value once available https://github.com/duckdb/duckdb/pull/14613
 		// FIXME: for other types: implement NamedValueChecker to support custom data types.
 		name := typeToStringMap[t]
 		return mapping.StateError, addIndexToError(unsupportedTypeError(name), n+1)
